5428d42aa13f9b6e00c93e336c3562eedefead98
sureshbasappa <reach2sureshb@gmail.com>
2021-05-25 10:44:16 +0300

    End of Course

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -26,6 +26,7 @@ currentUsers$ = this.currentUserSource.asObservable();
         if(user){
           this.setCurrentUser(user);
           this.presence.createHubConnection(user);
+          
         }
       })
     )
