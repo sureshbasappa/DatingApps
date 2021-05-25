12115e24b2b155e46118889a956e9ed73b839e13
sureshbasappa <reach2sureshb@gmail.com>
2021-05-25 08:44:18 +0300

    First commit

--- client/src/app/_interceptors/looding.interceptor.ts
+++ client/src/app/_interceptors/looding.interceptor.ts
@@ -17,7 +17,6 @@ export class LoodingInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     this.bussyService.bussy();
     return next.handle(request).pipe(
-      delay(1000),
       finalize(()=>{
         this.bussyService.idle();
       })
